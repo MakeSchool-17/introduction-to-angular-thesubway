d00e38f71dc9b73c078f8b5149cc8677214bc24b
Daniel Hoang <thesubway@gmail.com>
2016-02-10 16:32:48 -0800

    Add pokemon and display them in index file

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 angular.module('redditApp', [])
 .controller("MainCtrl", ['$scope', function($scope){
-
-  }]);
+    $scope.pokemon = [
+  {
+    ndex: 25,
+    name: 'Pikachu',
+    type: 'Electric'
+  },
+  {
+    ndex: 10,
+    name: 'Caterpie',
+    type: 'Bug'
+  },
+  {
+    ndex: 39,
+    name: 'Jigglypuff',
+    type: 'Fairy'
+  },
+  {
+    ndex: 94,
+     name: 'Gengar',
+    type: 'Ghost'
+  },
+  {
+    ndex: 143,
+    name: 'Snorlax',
+    type: 'Normal'
+  }
+];
+}]);
