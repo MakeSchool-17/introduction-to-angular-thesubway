b8267a97de3c8889a1245781b39b75e68a503270
Daniel Hoang <thesubway@gmail.com>
2016-02-15 14:52:14 -0800

    Use postform to check validation

--- app.js
+++ app.js
@@ -26,21 +26,17 @@ angular.module('redditApp', [])
   $scope.tournament = { title: "", format: 0 };
 
   $scope.createPost = function() {
-    var valid = true;
     var errors = "";
+    if ($scope.postForm.$valid == false) {
+        alert("Please fill out correct formats");
+        return false;
+    }
     if ($scope.post == null) {
         //this one requires immediate-break to avoid crashes.
         alert("Please fill out required fields");
         return false;
     }
-    if (!$scope.post.hasOwnProperty('title') || !$scope.post.hasOwnProperty('format')) {
-        valid = false;
-        errors = "Please fill out required fields";
-    }
-    if (valid == false) {
-        alert(errors);
-        return false;
-    }
+    //if (!$scope.post.hasOwnProperty('title') || !$scope.post.hasOwnProperty('format'))
     $scope.tournament = { title: $scope.post.title, format: $scope.post.format};
     $scope.tournaments.push($scope.tournament);
     $scope.tournament = { name: "", format: 0 };
